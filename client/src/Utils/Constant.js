880d2583101ea07528b2ed54dd03b0976288a283
Shourya-Sinha <shouryasinha.c@gmail.com>
2024-10-05 16:36:21 +0700

    update live server url

--- client/src/Utils/Constant.js
+++ client/src/Utils/Constant.js
@@ -1,5 +1,5 @@
 // export const HOST = import.meta.env.VITE_SERVER_URL;
-export const HOST = 'http://localhost:8747';
+export const HOST = 'https://lockchat-app-live.onrender.com';
 
 export const AUTH_ROUTES = 'api/auth';
 export const SIGNUP_ROUTE = `${AUTH_ROUTES}/signup`;
